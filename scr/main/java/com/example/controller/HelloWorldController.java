1f106c114d9b3f6c7751e1f66315784eb5d35648
root <root@ip-172-31-38-151.eu-west-3.compute.internal>
2023-12-18 10:09:55 +0000

    1

--- scr/main/java/com/example/controller/HelloWorldController.java
+++ scr/main/java/com/example/controller/HelloWorldController.java
@@ -9,11 +9,11 @@ public class HelloWorldController {
 	
 	@RequestMapping("/")
 	public String sayHello() {
-		return "Hello, This app is created for Demo purpose to client."
+		return "Welcome to Technical Cloud Knowledge India"
 			+
 			"\n"
 			+
-			"This app has been deployed using Pipeline (v0.2)"
+			"MD Name Of Sanjay Dahiya"
 			+
 			"\n";
 	}
